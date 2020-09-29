8d027b77879a047e7dc7080f30a1e840ca43cefc
Tjoakta04130 <8thonayah0@gmail.com>
2020-09-29 13:20:16 +0900

    検索は別エンドポイントで行うように修正

---
+++ api/src/main/java/web_diary/api/application/controller/SearchController.java
@@ -0,0 +1,36 @@
+package web_diary.api.application.controller;
+
+import java.util.List;
+
+import org.springframework.validation.BindingResult;
+import org.springframework.http.HttpStatus;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestController;
+
+import lombok.NonNull;
+import lombok.RequiredArgsConstructor;
+import web_diary.api.application.resource.SearchBody;
+import web_diary.api.domain.model.Diary;
+import web_diary.api.domain.service.DiaryService;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping(path = "web_diary/search")
+public class SearchController {
+  
+  @NonNull
+  private final DiaryService diaryService;
+
+  @PostMapping()
+  @ResponseStatus(HttpStatus.OK)
+  public List<Diary> search(@RequestBody @Validated SearchBody searchBody, BindingResult result) {
+    return this.diaryService.find_by_multi_condition(
+      searchBody.to_domain_diary(),
+      searchBody.get_end_priod()
+    );
+  }
+}
