bc88b52b373b9e86f4a08aff30dc892a4e2b62ee
Tjoakta04130 <8thonayah0@gmail.com>
2020-10-27 07:25:26 +0900

    フロント部分のデプロイ

---
+++ api/src/main/resources/static/precache-manifest.0d5a9345713909c5885cab002d02ce64.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0f905149bbbc608905371569509a8432",
+    "url": "/index.html"
+  },
+  {
+    "revision": "2a5f161e543bd629a866",
+    "url": "/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "ba1433191e3ec1911441",
+    "url": "/static/js/2.1c2aad62.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/static/js/2.1c2aad62.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2a5f161e543bd629a866",
+    "url": "/static/js/main.6b385208.chunk.js"
+  },
+  {
+    "revision": "9fd970e1f25c0cd7eb0a",
+    "url": "/static/js/runtime-main.470b33ba.js"
+  }
+]);
